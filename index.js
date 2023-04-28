fd9394774b4095ce60d47d143a5be1142dffbb92
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-04-28 00:30:51 -0400

    Lab Completed

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Add your code here
+
+
+const userName= "binilnitk";
+const mailid = "binilreni7@gmail.com";
+
+
+
+function submitData(userName,mailid)
+{
+     const createNew = document.querySelector("body");
+     console.log(createNew);
+
+    const configurationObject = {
+
+        method : "POST" ,
+        headers : {
+            "Content-Type" : "application/json",
+             "Accept" : "application/json",
+        },
+        body : JSON.stringify({
+            name : userName,
+            email : mailid
+        })
+    };
+
+     return (fetch("http://localhost:3000/users",configurationObject)
+     .then(function(response)
+     { 
+        return (response.json());
+     })
+     .then(function(data) {
+        createNew.innerHTML = `
+        <h1 id="id">${data.name}</h1>  
+        <h1 id="userDetails">${data.id}</h1>  
+        <h1 id="emailDetails">${data.email}</h1>  
+        `
+     })
+     .catch(function(error) {
+        const msg=document.getElementById("emailDetails");
+        msg.innerHTML=`
+        <h1>${error.message}</h1> 
+        `
+     }))
+     
+}
+
+
+submitData(userName,mailid);
+
+
+
+
+
+
+
+
+
+
+
+
+
